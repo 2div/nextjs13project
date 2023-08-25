cf881a8cbced292cf059c99b56e588c5a691c8b0
2div <mhersi819@gamil.com>
2023-08-25 23:14:33 +0300

    Updated

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -15,7 +15,6 @@ async function getData() {
 
 export default async function BlogPage() {
     const data = await getData()
-    console.log(process.env.PUBLIC_DOMAIN)
     const items = data.items ? [...data.items] :[]
     return <main>
         <h1>Hello world</h1>
